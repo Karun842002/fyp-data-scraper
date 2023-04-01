7b9d43db669721121b0d92d93c283872c7bdb504
karun <karun19049@cse.ssn.edu.in>
2023-04-01 11:06:16 +0530

    react app

--- demo/demo/src/App.js
+++ demo/demo/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
+import CountUp from 'react-countup'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import axios from 'axios'
 function App() {
+  const [confidence, setConfidence] = useState([0,50])
+  
+  function predict() {
+    setConfidence([confidence[1], 0])
+    let headline = document.getElementById("input").nodeValue
+    let res = axios.get(`http://localhost:5000/predict?sentence=${headline}`)
+    setConfidence([0, res.confidence])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="wrapper">
+        <div className='header'>FactFinder <FontAwesomeIcon icon={'magnifying-glass'} /></div>
+        <div id="form-wrapper">
+        <form onSubmit={predict}>
+          <div className='form-element input-element'><input type="text" id="input" placeholder="HEADLINE" name="sentence"></input></div>
+          <div className='form-element counter'><CountUp start={confidence[0]} end={confidence[1]} decimals={4}></CountUp>%</div>
+          <div className='form-element'><button type="submit" className='button-85'>Predict</button></div>
+        </form>
+        </div>
+      </div>
     </div>
   );
 }
