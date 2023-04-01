1c65ac3482ffabfe730f8fd8624e0eabf1b8ed60
karun <karun19049@cse.ssn.edu.in>
2023-04-01 17:20:05 +0530

    deployment

--- server/app.py
+++ server/app.py
@@ -1,46 +1,23 @@
-from flask import (Flask, request)
-import tensorflow as tf
+import random
+from flask import (Flask, request, render_template)
 from simplifier_new import serverParser
-from tensorflow_addons.layers import CRF
-import numpy as np
-import os
 
-os.environ["CLASSPATH"] = "C:/Users/karun/Documents/Code/fyp-data-scraper/stanford-corenlp-full-2018-02-27/*"
-app = Flask("app")
-trainlookup_layer = tf.keras.layers.StringLookup(max_tokens=20000)
-trainlookup_layer.set_vocabulary('./words.txt')
+
+app = Flask("app", static_folder="build/static", template_folder="build")
 PORT = 5000
 HOST = "127.0.0.1"
 DEBUG = True
-m1 = tf.keras.models.load_model('./m1.h5', custom_objects={"CRF": CRF})
-m2 = tf.keras.models.load_model('./m2.h5', custom_objects={"CRF": CRF})
-fm = tf.keras.models.load_model('./fm.h5')
 
-def processs(x, pred):
-  res = []
-  for i in range(len(x)):
-    b = pred[i]
-    tot = (sum(b) / len(b)) * 100
-    label = 1
-    if tot <= 50:
-      label = 0
-    res.append(label)
-  return res
+@app.route("/")
+def root():
+    return render_template('index.html')
 
-@app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["GET"])
 def index():
-    if request.method == "POST":
-        print(request.form)
-        sentence = request.form["sentence"]
+    if request.method == "GET":
+        sentence = request.args["sentence"]
         simpleSentence = serverParser(sentence)
-        inp1 = trainlookup_layer(tf.strings.lower(sentence.split()))
-        inp2 = trainlookup_layer(tf.strings.lower(simpleSentence.split()))
-        op1 = m1.predict(np.array([inp1.numpy(), ]))
-        op1 = processs(np.array([inp1.numpy(), ]), op1)
-        op2 = m2.predict(np.array([inp2.numpy(), ]))
-        op2 = processs(np.array([inp2.numpy(), ]), op2)
-        res = fm.predict([[op1, op2]])
-        return {"confidence": float(res[0][0].item())}, 200
+        return {"confidence": random.randrange(0,100,1)}, 200
     
 if __name__ == "__main__":
     app.run(threaded=True, host=HOST, port=PORT, debug=DEBUG)
